ff642757ac7c28f735e978eb84eaefcc1e7e25fe
Xuyuan Pang <xuyuanp@gmail.com>
2021-07-20 23:42:20 +0800

    Add 32

---
+++ 32.longest-valid-parentheses.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode id=32 lang=python3
+#
+# [32] Longest Valid Parentheses
+#
+# https://leetcode.com/problems/longest-valid-parentheses/description/
+#
+# algorithms
+# Hard (30.20%)
+# Likes:    5657
+# Dislikes: 198
+# Total Accepted:    395.2K
+# Total Submissions: 1.3M
+# Testcase Example:  '"(()"'
+#
+# Given a string containing just the characters '(' and ')', find the length of
+# the longest valid (well-formed) parentheses substring.
+#
+#
+# Example 1:
+#
+#
+# Input: s = "(()"
+# Output: 2
+# Explanation: The longest valid parentheses substring is "()".
+#
+#
+# Example 2:
+#
+#
+# Input: s = ")()())"
+# Output: 4
+# Explanation: The longest valid parentheses substring is "()()".
+#
+#
+# Example 3:
+#
+#
+# Input: s = ""
+# Output: 0
+#
+#
+#
+# Constraints:
+#
+#
+# 0 <= s.length <= 3 * 10^4
+# s[i] is '(', or ')'.
+#
+#
+#
+
+# @lc code=start
+from collections import defaultdict
+
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        res = 0
+        dp = defaultdict(int)
+
+        for i in range(1, len(s)):
+            if s[i] != ')':
+                continue
+            if s[i-1] == '(':
+                dp[i] = dp[i-2] + 2
+            else:
+                mirror = i - dp[i-1] - 1
+                if mirror >= 0 and s[mirror] == '(':
+                    dp[i] = dp[i-1] + dp[mirror-1] + 2
+            res = max(res, dp[i])
+
+        return res
+
+# @lc code=end
+
+if __name__ == "__main__":
+    sol = Solution()
+    cases = [
+        ('', 0),
+        ('(()', 2),
+        (')()())', 4),
+        ('(()))())(', 4)
+    ]
+    for s, want in cases:
+        got = sol.longestValidParentheses(s)
+        if got != want:
+            print(f'Failed => args: {s}; want: {want}, but got: {got}')
+            break
+    else:
+        print('All Passed')
