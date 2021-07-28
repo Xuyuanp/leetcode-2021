72db7a97f8e786127e536c69bfb112c7ca55907e
Xuyuan Pang <xuyuanp@gmail.com>
2021-07-29 00:02:25 +0800

    Add 81

---
+++ 0081.search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,105 @@
+#
+# @lc app=leetcode id=81 lang=python3
+#
+# [81] Search in Rotated Sorted Array II
+#
+# https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+#
+# algorithms
+# Medium (33.89%)
+# Likes:    2453
+# Dislikes: 605
+# Total Accepted:    321.1K
+# Total Submissions: 945.6K
+# Testcase Example:  '[2,5,6,0,0,1,2]\n0'
+#
+# There is an integer array nums sorted in non-decreasing order (not
+# necessarily with distinct values).
+#
+# Before being passed to your function, nums is rotated at an unknown pivot
+# index k (0 <= k < nums.length) such that the resulting array is [nums[k],
+# nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For
+# example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become
+# [4,5,6,6,7,0,1,2,4,4].
+#
+# Given the array nums after the rotation and an integer target, return true if
+# target is in nums, or false if it is not in nums.
+#
+# You must decrease the overall operation steps as much as possible.
+#
+#
+# Example 1:
+# Input: nums = [2,5,6,0,0,1,2], target = 0
+# Output: true
+# Example 2:
+# Input: nums = [2,5,6,0,0,1,2], target = 3
+# Output: false
+#
+#
+# Constraints:
+#
+#
+# 1 <= nums.length <= 5000
+# -10^4 <= nums[i] <= 10^4
+# nums is guaranteed to be rotated at some pivot.
+# -10^4 <= target <= 10^4
+#
+#
+#
+# Follow up: This problem is similar to Search in Rotated Sorted Array, but
+# nums may contain duplicates. Would this affect the runtime complexity? How
+# and why?
+#
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        n = len(nums)
+        left, right = 0, n-1
+        while left <= right:
+            while left < right and nums[left] == nums[left+1]:
+                left += 1
+            while right > left and nums[right] == nums[right-1]:
+                right -= 1
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return True
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return False
+
+# @lc code=end
+if __name__ == '__main__':
+    sol = Solution()
+    methods = [name for name in dir(sol) if not name.startswith('__')]
+    for method in methods:
+        print(f'Testing {method}:')
+        fn = getattr(sol, method)
+        cases = [
+            ([[1], 1], True),
+            ([[1], 0], False),
+            ([[1, 1], 1], True),
+            ([[1, 1], 0], False),
+            ([[2,5,6,0,0,1,2], 0], True),
+            ([[2,5,6,0,0,1,2], 3], False),
+            ([[2,2,2,3,2,2,2], 3], True),
+            ([[1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1], 2], True)
+        ]
+        for args, want in cases:
+            got = fn(*args)
+            if want != got:
+                print(f'  Failed => args: {args}; want: {want}, but got: {got}')
+                break
+        else:
+            print('  All Passed')
+        print()
