a459078ecdf42738dba227b96b03850b3aea95f4
Xuyuan Pang <xuyuanp@gmail.com>
2021-07-30 01:11:08 +0800

    Add 704

---
+++ 0704.binary-search.py
@@ -0,0 +1,125 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+# https://leetcode.com/problems/binary-search/description/
+#
+# algorithms
+# Easy (54.74%)
+# Likes:    1621
+# Dislikes: 70
+# Total Accepted:    298.5K
+# Total Submissions: 544.4K
+# Testcase Example:  '[-1,0,3,5,9,12]\n9'
+#
+# Given an array of integers nums which is sorted in ascending order, and an
+# integer target, write a function to search target in nums. If target exists,
+# then return its index. Otherwise, return -1.
+#
+# You must write an algorithm with O(log n) runtime complexity.
+#
+#
+# Example 1:
+#
+#
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+#
+#
+# Example 2:
+#
+#
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= nums.length <= 10^4
+# -10^4 < nums[i], target < 10^4
+# All the integers in nums are unique.
+# nums is sorted in ascending order.
+#
+#
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def search3(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left, right = 0, len(nums)-1
+        while left + 1 < right:
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid
+            else:
+                right = mid
+        if nums[left] == target:
+            return left
+        if nums[right] == target:
+            return right
+        return -1
+
+    def search2(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid+1
+            else:
+                right = mid
+        if left < len(nums) and nums[left] == target:
+            return left
+        return -1
+
+    def search1(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+# @lc code=end
+if __name__ == '__main__':
+    sol = Solution()
+    methods = [name for name in dir(sol) if not name.startswith('__')]
+    for method in methods:
+        print(f'Testing {method}:')
+        fn = getattr(sol, method)
+        cases = [
+            ([[], 1], -1),
+            ([[1], 1], 0),
+            ([[1], 2], -1),
+            ([[1,2,3], 0], -1),
+            ([[1,2,3], 1], 0),
+            ([[1,2,3], 2], 1),
+            ([[1,2,3], 3], 2),
+            ([[1,2,3], 4], -1),
+            ([[1,2,3], 4], -1),
+            ([[-1,0,3,5,9,12], 9], 4),
+            ([[-1,0,3,5,9,12], 2], -1),
+        ]
+        for args, want in cases:
+            got = fn(*args)
+            if want != got:
+                print(f'  Failed => args: {args}; want: {want}, but got: {got}')
+                break
+        else:
+            print('  All Passed')
+        print()
