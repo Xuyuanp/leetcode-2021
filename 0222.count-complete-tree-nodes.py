cfc94206acc3542238bbeac62af5762b8f8905fb
Xuyuan Pang <xuyuanp@gmail.com>
2021-08-02 23:13:29 +0800

    Add 222

---
+++ 0222.count-complete-tree-nodes.py
@@ -0,0 +1,102 @@
+#
+# @lc app=leetcode id=222 lang=python3
+#
+# [222] Count Complete Tree Nodes
+#
+# https://leetcode.com/problems/count-complete-tree-nodes/description/
+#
+# algorithms
+# Medium (50.76%)
+# Likes:    3260
+# Dislikes: 271
+# Total Accepted:    314.5K
+# Total Submissions: 614.8K
+# Testcase Example:  '[1,2,3,4,5,6]'
+#
+# Given the root of a complete binary tree, return the number of the nodes in
+# the tree.
+#
+# According to Wikipedia, every level, except possibly the last, is completely
+# filled in a complete binary tree, and all nodes in the last level are as far
+# left as possible. It can have between 1 and 2^h nodes inclusive at the last
+# level h.
+#
+# Design an algorithm that runs in less than O(n) time complexity.
+#
+#
+# Example 1:
+#
+#
+# Input: root = [1,2,3,4,5,6]
+# Output: 6
+#
+#
+# Example 2:
+#
+#
+# Input: root = []
+# Output: 0
+#
+#
+# Example 3:
+#
+#
+# Input: root = [1]
+# Output: 1
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the tree is in the range [0, 5 * 10^4].
+# 0 <= Node.val <= 5 * 10^4
+# The tree is guaranteed to be complete.
+#
+#
+#
+from structures import TreeNode
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # O(log(n)*log(n)), O(log(n))
+    def countNodes(self, root: TreeNode) -> int:
+        def depth(node: TreeNode) -> int:
+            if not node:
+                return 0
+            return 1 + depth(node.left)
+
+        if not root:
+            return 0
+
+        left_depth = depth(root.left)
+        right_depth = depth(root.right)
+        # count of nodes in a full tree = 2^depth-1
+        # so, total count of the full sub tree and the root is 2^depth
+        if left_depth == right_depth:
+            return (1<<left_depth) + self.countNodes(root.right)
+        else:
+            return (1<<right_depth) + self.countNodes(root.left)
+
+# @lc code=end
+if __name__ == '__main__':
+    sol = Solution()
+    methods = [name for name in dir(sol) if not name.startswith('__')]
+    for method in methods:
+        print(f'Testing {method}:')
+        fn = getattr(sol, method)
+        cases = [([TreeNode.from_list(list(range(n)))], n) for n in range(1, 100)]
+        for args, want in cases:
+            got = fn(*args)
+            if want != got:
+                print(f'  Failed => args: {args}; want: {want}, but got: {got}')
+                break
+        else:
+            print('  All Passed')
+        print()
