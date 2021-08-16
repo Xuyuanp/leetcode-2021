2be1aff88cd64b1b455e29caa07bbd7afd83631e
Xuyuan Pang <xuyuanp@gmail.com>
2021-08-16 19:08:35 +0800

    Add 1046

---
+++ 1046.last-stone-weight.py
@@ -0,0 +1,104 @@
+#
+# @lc app=leetcode id=1046 lang=python3
+#
+# [1046] Last Stone Weight
+#
+# https://leetcode.com/problems/last-stone-weight/description/
+#
+# algorithms
+# Easy (62.47%)
+# Likes:    1589
+# Dislikes: 40
+# Total Accepted:    183.3K
+# Total Submissions: 293K
+# Testcase Example:  '[2,7,4,1,8,1]'
+#
+# You are given an array of integers stones where stones[i] is the weight of
+# the i^th stone.
+#
+# We are playing a game with the stones. On each turn, we choose the heaviest
+# two stones and smash them together. Suppose the heaviest two stones have
+# weights x and y with x <= y. The result of this smash is:
+#
+#
+# If x == y, both stones are destroyed, and
+# If x != y, the stone of weight x is destroyed, and the stone of weight y has
+# new weight y - x.
+#
+#
+# At the end of the game, there is at most one stone left.
+#
+# Return the smallest possible weight of the left stone. If there are no stones
+# left, return 0.
+#
+#
+# Example 1:
+#
+#
+# Input: stones = [2,7,4,1,8,1]
+# Output: 1
+# Explanation:
+# We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+# we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+# we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+# we combine 1 and 1 to get 0 so the array converts to [1] then that's the
+# value of the last stone.
+#
+#
+# Example 2:
+#
+#
+# Input: stones = [1]
+# Output: 1
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= stones.length <= 30
+# 1 <= stones[i] <= 1000
+#
+#
+#
+import heapq
+from typing import List
+
+# @lc code=start
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        heap = [-x for x in stones]
+        heapq.heapify(heap)
+        while len(heap) > 1:
+            x = heapq.heappop(heap)
+            y = heapq.heappop(heap)
+            heapq.heappush(heap, x-y)
+
+        return -heap[0]
+
+# @lc code=end
+def test():
+    sol = Solution()
+    methods = [name for name in dir(sol) if not name.startswith('__')]
+    for method in methods:
+        print(f'Testing {method}:')
+        func = getattr(sol, method)
+        cases = [
+            ([[1]], 1),
+            ([[1,1]], 0),
+            ([[1,2]], 1),
+            ([[1,2,3]], 0),
+            ([[2,7,4,1,8,1]], 1),
+        ]
+        for args, want in cases:
+            got = func(*args)
+            if want != got:
+                print(f'  Failed => args: {args}; want: {want}, but got: {got}')
+                break
+        else:
+            print('  All Passed')
+        print()
+
+
+if __name__ == '__main__':
+    test()
