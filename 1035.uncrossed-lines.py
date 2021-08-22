931480855975447ebdbeb49cff1fc7ec37d0e13c
Xuyuan Pang <xuyuanp@gmail.com>
2021-08-22 22:19:55 +0800

    Update 1035

--- 1035.uncrossed-lines.py
+++ 1035.uncrossed-lines.py
@@ -82,6 +82,23 @@ class Solution:
 
         return dp[m][n]
 
+    # O(m*n), O(min(m, n)).
+    # dp[i] -> next_dp
+    # dp[i-1] -> dp
+    def maxUncrossedLines1(self, nums1: List[int], nums2: List[int]) -> int:
+        m, n = len(nums1), len(nums2)
+        dp = [0] * (n+1)
+        for i in range(1, m+1):
+            next_dp = [0] * (n+1)
+            for j in range(1, n+1):
+                if nums1[i-1] == nums2[j-1]:
+                    next_dp[j] = dp[j-1]+1
+                else:
+                    next_dp[j] = max(dp[j], next_dp[j-1])
+            dp = next_dp
+
+        return dp[n]
+
 # @lc code=end
 def test():
     sol = Solution()
