28322ed2263ece76dd77253485165c207056ee38
Xuyuan Pang <xuyuanp@gmail.com>
2021-08-23 16:11:34 +0800

    Update 712

--- 0712.minimum-ascii-delete-sum-for-two-strings.py
+++ 0712.minimum-ascii-delete-sum-for-two-strings.py
@@ -62,20 +62,18 @@ class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         m, n = len(s1), len(s2)
         dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            dp[i][0] = ord(s1[i-1]) + dp[i-1][0]
+        for j in range(1, n+1):
+            dp[0][j] = ord(s2[j-1]) + dp[0][j-1]
 
-        for i in range(m+1):
-            for j in range(n+1):
-                if i == j == 0:
-                    pass
-                elif i == 0:
-                    dp[i][j] = ord(s2[j-1]) + dp[i][j-1]
-                elif j == 0:
-                    dp[i][j] = ord(s1[i-1]) + dp[i-1][j]
-                elif s1[i-1] == s2[j-1]:
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s1[i-1] == s2[j-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
                     dp[i][j] = min(
-                        ord(s1[i-1])+dp[i-1][j],
+                        ord(s1[i-1]) + dp[i-1][j],
                         ord(s2[j-1]) + dp[i][j-1]
                     )
 
@@ -87,22 +85,19 @@ class Solution:
             s1, s2 = s2, s1
         m, n = len(s1), len(s2)
         dp = [0] * (n+1)
+        for j in range(1, n+1):
+            dp[j] = ord(s2[j-1]) + dp[j-1]
 
-        for i in range(m+1):
+        for i in range(1, m+1):
             next_dp = [0]*(n+1)
-            for j in range(n+1):
-                if i == j == 0:
-                    pass
-                elif i == 0:
-                    next_dp[j] = ord(s2[j-1]) + next_dp[j-1]
-                elif j == 0:
-                    next_dp[j] = ord(s1[i-1]) + dp[j]
-                elif s1[i-1] == s2[j-1]:
+            next_dp[0] = ord(s1[i-1]) + dp[0]
+            for j in range(1, n+1):
+                if s1[i-1] == s2[j-1]:
                     next_dp[j] = dp[j-1]
                 else:
                     next_dp[j] = min(
                         ord(s1[i-1]) + dp[j],
-                        ord(s2[j-1])+next_dp[j-1],
+                        ord(s2[j-1]) + next_dp[j-1],
                     )
             dp = next_dp
 
