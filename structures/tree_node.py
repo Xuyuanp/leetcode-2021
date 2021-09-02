361cdb76f8204eba85af668e45dd0ce895b1a4b2
Xuyuan Pang <xuyuanp@gmail.com>
2021-09-02 23:01:45 +0800

    Update treenode

--- structures/tree_node.py
+++ structures/tree_node.py
@@ -1,37 +1,23 @@
-from typing import List, Optional
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, List, Optional
 from collections import deque
 from itertools import zip_longest
 
+@dataclass
 class TreeNode:
-    def __init__(self, val=0, left: 'TreeNode' = None, right: 'TreeNode' = None):
-        self.val = val
-        self.left = left
-        self.right = right
+    val: Any
+    left: Optional[TreeNode] = None
+    right: Optional[TreeNode] = None
 
     def __repr__(self) -> str:
-        def helper(node: TreeNode):
-            q = deque([(node, 0)])
-            while q:
-                node, level = q.popleft()
-                yield node.val, level
-                if node.left:
-                    q.append((node.left, level+1))
-                if node.right:
-                    q.append((node.right, level+1))
-
-        return str(list(helper(self)))
-
-    def __eq__(self, o) -> bool:
-        def eq(n1, n2) -> bool:
-            if n1 is None and n2 is None:
-                return True
-            if n1 and n2:
-                return n1.val == n2.val and n1.left == n2.left and n1.right == n2.right
-            return False
-        return eq(self, o)
+        if not self.left and not self.right:
+            return f'{{ {self.val} }}'
+        return f'{{ {self.val} [{self.left}, {self.right}] }}'
 
     @staticmethod
-    def from_list(vals: List[int]) -> Optional['TreeNode']:
+    def from_list(vals: List[int]) -> Optional[TreeNode]:
         if not vals:
             return None
         root = TreeNode(vals[0])
