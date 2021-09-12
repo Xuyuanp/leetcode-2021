640574701c33c7d278374664f5c328b12213a127
Xuyuan Pang <xuyuanp@gmail.com>
2021-09-12 13:33:39 +0800

    Add 947

---
+++ 0947.most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,140 @@
+#
+# @lc app=leetcode id=947 lang=python3
+#
+# [947] Most Stones Removed with Same Row or Column
+#
+# https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/description/
+#
+# algorithms
+# Medium (55.97%)
+# Likes:    1884
+# Dislikes: 478
+# Total Accepted:    83.4K
+# Total Submissions: 148.8K
+# Testcase Example:  '[[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]'
+#
+# On a 2D plane, we place n stones at some integer coordinate points. Each
+# coordinate point may have at most one stone.
+#
+# A stone can be removed if it shares either the same row or the same column as
+# another stone that has not been removed.
+#
+# Given an array stones of length n where stones[i] = [xi, yi] represents the
+# location of the i^th stone, return the largest possible number of stones that
+# can be removed.
+#
+#
+# Example 1:
+#
+#
+# Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+# Output: 5
+# Explanation: One way to remove 5 stones is as follows:
+# 1. Remove stone [2,2] because it shares the same row as [2,1].
+# 2. Remove stone [2,1] because it shares the same column as [0,1].
+# 3. Remove stone [1,2] because it shares the same row as [1,0].
+# 4. Remove stone [1,0] because it shares the same column as [0,0].
+# 5. Remove stone [0,1] because it shares the same row as [0,0].
+# Stone [0,0] cannot be removed since it does not share a row/column with
+# another stone still on the plane.
+#
+#
+# Example 2:
+#
+#
+# Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+# Output: 3
+# Explanation: One way to make 3 moves is as follows:
+# 1. Remove stone [2,2] because it shares the same row as [2,0].
+# 2. Remove stone [2,0] because it shares the same column as [0,0].
+# 3. Remove stone [0,2] because it shares the same row as [0,0].
+# Stones [0,0] and [1,1] cannot be removed since they do not share a row/column
+# with another stone still on the plane.
+#
+#
+# Example 3:
+#
+#
+# Input: stones = [[0,0]]
+# Output: 0
+# Explanation: [0,0] is the only stone on the plane, so you cannot remove
+# it.
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= stones.length <= 1000
+# 0 <= xi, yi <= 10^4
+# No two stones are at the same coordinate point.
+#
+#
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    # O(n^2), O(n)
+    def removeStones(self, stones: List[List[int]]) -> int:
+        visited = set()
+
+        def dfs(i: int):
+            visited.add(i)
+            for j, (row, col) in enumerate(stones):
+                if j not in visited:
+                    if stones[i][0] == row or stones[i][1] == col:
+                        dfs(j)
+
+        islands = 0
+        for i in range(len(stones)):
+            if i not in visited:
+                dfs(i)
+                islands += 1
+
+        return len(stones) - islands
+
+    # O(n), O(n)
+    def removeStones1(self, stones: List[List[int]]) -> int:
+        ufs = {} # union find set
+
+        def find(x):
+            if x != ufs.setdefault(x, x):
+                ufs[x] = find(ufs[x])
+            return ufs[x]
+
+        def union(x, y):
+            rx, ry = find(x), find(y)
+            ufs[rx] = ry
+
+        for i, j in stones:
+            union(i, ~j)
+
+        return len(stones) - len({find(x) for x in ufs})
+
+
+# @lc code=end
+def test():
+    sol = Solution()
+    methods = [name for name in dir(sol) if not name.startswith('__')]
+    for method in methods:
+        print(f'Testing {method}:')
+        func = getattr(sol, method)
+        cases = [
+            ([[[0,0]]], 0),
+            ([[[0,0],[0,2],[1,1],[2,0],[2,2]]], 3),
+            ([[[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]], 5),
+        ]
+        for args, want in cases:
+            got = func(*args)
+            if want != got:
+                print(f'  Failed => args: {args}; want: {want}, but got: {got}')
+                break
+        else:
+            print('  All Passed')
+        print()
+
+
+if __name__ == '__main__':
+    test()
+
